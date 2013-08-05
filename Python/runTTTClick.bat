755f03514a1e28d1133d95eac570d58dfdee1e9d
Civilwargeeky <civilwargeeky@yahoo.com>
2013-08-04 21:04:48 -0500

    Really simple TTT bat

---
+++ Python/runTTTClick.bat
@@ -0,0 +1,9 @@
+@echo off
+set currLoc=%cd%
+set pythonBank=TTTClick.py
+if exist %pythonBank% (
+  echo Found File, starting now, if you can read this, something has gone wrong
+  start python %pythonBank%
+) else (
+  echo Could not find %pythonBank%, try again
+)
