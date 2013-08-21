912795d66a8c051905df5505dbd5d9e80d6a7491
civil <civilwargeeky@yahoo.com>
2013-08-20 22:59:48 -0500

    Uggh. Some confusing Unknown problem

--- Python/SysPrint.py
+++ Python/SysPrint.py
@@ -2,21 +2,34 @@ import pygame
 
 if not pygame.font.get_init(): pygame.font.init()
 
-data = { "font": "Calibri", "size": 20, "color": (0,0,0), "antiAlias": False, "buffer": 5}
+data = { "screen": None, "font": "Calibri", "size": 20, "color": (0,0,0), "antiAlias": False, "buffer": 5}
+openLines = []
 currLine, maxLines = 1, 20
 fontObj = pygame.font.SysFont(data["font"],data["size"])
 
-def init(font, pageHeight, lines, color = data["color"], antiAlias = data["antiAlias"], buffer = data["buffer"]):
+def lineSet(line=1): #Sets the current line position
+  global currLine #What I'm changing :)
+  if 1<= line <= maxLines:
+    currLine = line
+    return True
+  return False
+
+def lineInc(num=1): #Changes the current line position up
+  if abs(num) < maxLines:
+    lineSet(currLine+num) or lineSet()
+    return True
+  return False
+
+def init(surfaceObject, lines, font = data["font"], color = data["color"], antiAlias = data["antiAlias"], buffer = data["buffer"]):
+  """The way this is set up, you give it your screen dimensions and the number of lines you want on this screen
+  (Optionally a whole bunch of other parameters), and it comes up with a font size for you."""
   global data, maxLines, fontObj
-  data["font"], data["color"], data["antiAlias"], data["buffer"] = font, color, antiAlias, buffer
-  maxLines, data["size"] = lines, int(pageHeight / lines) or 1
-  fontObj = pygame.font.SysFont(data["font"],data["size"])
+  data["screen"], data["font"], data["color"], data["antiAlias"], data["buffer"] = surfaceObject, font, color, antiAlias, buffer
+  maxLines, data["size"] = lines, (int(surfaceObject.get_size()[0] / lines) or 1) #Verify that 0 is height and not 1
+  print(data["font"])
+  print(data["size"])
+  fontObj = pygame.font.SysFont(data["font"], data["size"])
 
-def print(surfObj, toPrint, orientation = "left"):
-  global currLine
-  surfObj.blit(fontObj.render(toPrint if isinstance(toPrint,str) else repr(toPrint),data["antiAlias"], data["color"]),(0, (currLine-1) * data["size"] + data["buffer"]))
-  currLine = currLine + 1 if currLine < maxLines else 1
-
-def setLine(line = 1):
-  pass
-
+def print(toPrint, flag = None):
+  data["screen"].blit(fontObj.render(toPrint, data["antiAlias"], data["color"]),(0, (currLine-1) * data["size"] + data["buffer"]))
+  lineInc()
