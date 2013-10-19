8319dfc91c3279bc826c248d60009ff30eeac272
Civilwargeeky <civilwargeeky@yahoo.com>
2013-10-19 17:06:07 -0500

    Removed some files

--- Python/runTTTClick.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set currLoc=%cd%
-set pythonBank=TTTClick.py
-if exist %pythonBank% (
-  echo Found File, starting now, if you can read this, something has gone wrong
-  start python %pythonBank%
-) else (
-  echo Could not find %pythonBank%, try again
-)
