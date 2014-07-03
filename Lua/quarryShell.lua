24928482685be4208f09754c9eaf3db94e66258c
Civilwargeeky <civilwargeeky@yahoo.com>
2014-07-02 23:29:41 -0500

    Made a mockup of how the main interface will work. Next add classes to take care of menus. Current is really prone to forgetting commas ;p

--- Lua/quarryShell.lua
+++ Lua/quarryShell.lua
@@ -10,28 +10,57 @@ These will be registered in a config file specific to this program.]]
 globalTable = nil; _G.globalTable = {}; setmetatable(globalTable, {__index = getfenv(1)}); setfenv(1,globalTable)
 
 local fileNames = {
-  menuAPI = "menuAPI.lua"
+  workingDirectory = "", --For debug
+  workingDirectory = "quarryResources",
+  menuAPI = "menuAPI.lua",
 }
 local gettingAPI = {
-  version = "1.0.0"
-  pastebin = "4ptKaSQp"
+  version = "1.0.0",
+  pastebin = "4ptKaSQp",
 }
+--Setting directory
+if not fs.isDir(fileNames.workingDirectory) then
+  fs.makeDir(fileNames.workingDirectory)
+end
+shell.setDir(fileNames.workingDirectory)
+local function addDir(file, dir)
+  return (dir or shell.dir()) .. "/"..file
+end
 
---Basic screen functions
-  local doPrintDebug = true --For debugging messages
-  local function clearSet(x,y) term.clear(); term.setCursorPos(x or 1, y or 1) end
-  local function debug(...)
-    if doPrintDebug then
-      return print(...)
-    end
+
+--Screen and Debug functions
+local doPrintDebug = true --For debugging messages
+local function clearSet(x,y) term.clear(); term.setCursorPos(x or 1, y or 1) end
+local function debug(...)
+  if doPrintDebug then
+    return print(...)
   end
+end
+
+--Program functions
+local function exit(noClear)
+  if not noClear then
+    clearSet()
+  end
+  shell.setDir("")
+  error("",0)
+end
+local function copyTab(tab)
+  local toRet = {}
+  for a,b in pairs(tab) do
+    toRet[a] = b
+  end
+  return toRet
+end
 --Acquiring menu api
 local requiresDownload = false
-if not fs.exists(fileNames.menuAPI) then
+if not fs.exists(addDir(fileNames.menuAPI)) then
   requiresDownload = true
 else
-  local file = fs.open(fileNames.menuAPI,"w")
+  debug("API exists")
+  local file = fs.open(addDir(fileNames.menuAPI),"r")
   if not file or file.readAll():match("Version (%d+%.%d+%.%d+)") ~= gettingAPI.version then --If we don't have the proper version, redownload stuff
+    debug("Improper API Version, downloading latest")
     requiresDownload = true
   end
   if file then file.close() end --Stupid file might not exist stuff
@@ -41,13 +70,68 @@ if requiresDownload then
   if not http then --If we can't download it
     clearSet()
     print("HTTP API not enabled. For this program to work, go to my pastebin and download the menu api as ",fileNames.menuAPI)
-    error("",0)
+    exit(true)
   end
-  shell.run("pastebin get ",gettingAPI.pastebin)
-  if not fs.exists(fileNames.menuAPI) then --If pastebin get failed
+  if fs.exists(addDir(fileNames.menuAPI)) then fs.delete(addDir(fileNames.menuAPI)) end
+  shell.run("pastebin get ",gettingAPI.pastebin," ",fileNames.menuAPI) --No addDir because pastebin puts in working directory
+  if not fs.exists(addDir(fileNames.menuAPI)) then --If pastebin get failed
     print("API Failed to download.")
     print("Sorry, but this program cannot work without it. Try again later")
-    error("",0)
+    exit(true)
   end
   debug("API Successfully downloaded")
 end
+
+--Loading API
+local menu = {}
+os.run(menu, addDir(fileNames.menuAPI))
+if menu.menu then debug("Menu API loaded successfully") end
+
+
+local function printPause(wait, ...)
+  print(...)
+  sleep(wait)
+end
+  
+
+main = {
+  title = "Welcome to Quarry Shell",
+  { text = "Option 1",
+    action = nil,
+    child = {
+      title = "Inner test",
+      { text = "Sub 1",
+        action  = {printPause, 3, "This works!"},
+      },
+      { text = "Back",
+        action = "return",
+      }
+    }
+  },
+  { text = "Exit",
+    action = {exit},
+  }
+}
+
+
+local currentTab, parent = main
+while true do
+  local text, index = menu.menu(currentTab.title, "", currentTab, false)
+  local selection = currentTab[index]
+  if not selection.action then
+    parent = currentTab
+    currentTab = selection.child
+  elseif selection.action == "return" then
+    currentTab = parent or exit()
+  else
+    local tab = copyTab(selection.action) --Don't want to be popping from a menu
+    local func = table.remove(tab, 1) --Take out the function part
+    func(unpack(tab)) --Pass everything else as arguments
+  end
+  
+end
+
+
+
+
+exit()
