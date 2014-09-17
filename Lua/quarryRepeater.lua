53e8ea2a9da3321c86d49d76af5b1f8b2fa4a6e5
unknown <danielklinger15@SH12.SCS.SCSPK12.Org>
2014-09-17 10:30:33 -0500

    Should be done with repeater, now just needs testing

--- Lua/quarryRepeater.lua
+++ Lua/quarryRepeater.lua
@@ -13,18 +13,21 @@
   ]]
 --expected message format: {message, id, distance}
 
+--Config
 local doDebug = true --...
 local arbitraryNumber = 100 --How many messages to keep between deletions
+local saveFile = "QuarryRepeaterSave"
+local expectedFingerprint = "quarry"
+local acceptLegacy = true --This will auto-format old messages that come along
+--Init
 local sentMessages = {} --Table of received message ids
 local idMap = {} --List of random ids
-local expectedFingerprint = "quarry"
 local counter = 0
+local channels = {} --List of channels to open listen on
+local modem --The wireless modem
 
-local function debug(...)
-  if doDebug then
-    return print(...)
-  end
-end
+
+local function debug(...) if doDebug then return print(...) end end --Debug print
 
 local function newID()
   return math.random(1,2000000) --1 through 2 billion; close enough 
@@ -32,15 +35,45 @@ end
 local function addID(id)
     sentMessages[id] = true
     idMap[#idMap+1] =  id
+    counter = counter + 1
+end
+local function saveFile()
+  local file = fs.open(saveFile, "w")
+  file.write(textutils.unpack(channels))
+  file.write(counter)
+  return file.close()
+end
+local function openChannels(modem)
+  for a,b in pairs(channels) do
+    if not modem.isOpen(b) then
+      modem.open(b)
+    end
+  end
+end
+
+if fs.exists(saveFile) then
+  local file = fs.open(saveFile,"r")
+  channels = textutils.unserialize(file:read()) or print("Channels could not be read") or {}
+  counter = tonumber(file:read()) or print("Counter could not be read") or 0
+  print("Done reading save file")
+  file:close()
+end
+
+while not modem do
+  modem = peripheral.find("modem")
+  if modem then break end
+  print("Please attach a modem")
+  os.pullEvent("peripheral")
 end
 
 while true do
+  print("\nHit 'q' to quit, 'r' to remove channels, or any other key to add channels")
   local event, key, receivedFreq, replyFreq, received, dist = os.pullEvent()
+  term.clear()
+  term.setCursorPos(1,1)
   if event == "modem_message" then
-    term.clear()
-    term.setCursorPos(1,1)
     print("Modem Message Received")
-    if not type(received) == "table" then
+    if acceptLegacy and not type(received) == "table" then
       debug("Unformatted message, formatting for quarry")
       received = { message = received, id = newID(), distance = 0, fingerprint = expectedFingerprint}
     end
@@ -58,11 +91,51 @@ while true do
     end
 
     if #idMap > arbitraryNumber then --Purge messages to save memory
+      debug("Purging messages")
       for i=#idMap, 2, -1 do --Save one ID for most recent message
         sentMessages[idMap[i]] = nil
         idMap[i] = nil
       end
     end
+    
+    print("Messages Received: "..counter)
+    
+  elseif event == "char" then
+    if key == "q" then --Quitting
+      error("Quitting",0)
+    elseif key == "c" then --Removing Channels
+      print("Enter a comma seperated list of channels to remove")
+      local str = "" --Concantenate all the channels into one, maybe restructure this for sorting?
+      for i=1,#channels do
+        str = str..tostring(channels[i])..", "
+      end
+      print("Channels: ",str:sub(1,-2)) --Sub for removing comma and space
+      local input = io.read()
+      local toRemove = {} --We have to use this table because user list will not be in reverse numerical order
+      for num in input:gmatch("%d+") do
+        for a, b in pairs(channels) do
+          if b == tonumber(num) then
+            debug("Removing ",b)
+            table.insert(toRemove, a, 1) --This way it will go from the back of the table
+            modem.close(b)
+            break --No use checking the rest of the table for this number
+          end
+        end
+      end
+      for i=1, #toRemove do --So that we arent
+        table.remove(channels, toRemove[i])
+      end
+    else  --Adding Channels
+      print("What channels would you like to open. Enter a comma-separated list\n")
+      local input = io.read()
+      for num in input:gmatch("%d+") do
+        if num >= 1 and num <= 65535 then
+          table.insert(channels, tonumber(num))
+        end
+      end
+    end
+    saveFile()
+    openChannels()
     
   end
 end
