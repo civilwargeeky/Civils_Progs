f2aeebdd22aa729de581e2dfc274d69a216e7d78
unknown <DanielKlinger15@HS-BL10.SCS.SCSPK12.Org>
2014-09-17 07:57:00 -0500

    More repeater work

--- Lua/quarryRepeater.lua
+++ Lua/quarryRepeater.lua
@@ -13,22 +13,56 @@
   ]]
 --expected message format: {message, id, distance}
 
+local doDebug = true --...
 local arbitraryNumber = 100 --How many messages to keep between deletions
 local sentMessages = {} --Table of received message ids
 local idMap = {} --List of random ids
+local expectedFingerprint = "quarry"
+local counter = 0
+
+local function debug(...)
+  if doDebug then
+    return print(...)
+  end
+end
 
 local function newID()
   return math.random(1,2000000) --1 through 2 billion; close enough 
+end
+local function addID(id)
+    sentMessages[id] = true
+    idMap[#idMap+1] =  id
 end
 
 while true do
   local event, key, receivedFreq, replyFreq, received, dist = os.pullEvent()
   if event == "modem_message" then
+    term.clear()
+    term.setCursorPos(1,1)
+    print("Modem Message Received")
     if not type(received) == "table" then
-      received = { message = received, id = newID(), distance = dist}
+      debug("Unformatted message, formatting for quarry")
+      received = { message = received, id = newID(), distance = 0, fingerprint = expectedFingerprint}
     end
-    sentMessages[id] = true
-    table.insert(idMap, id)
+    
+    debug("Message Properties")
+    for a, b in pairs(received) do
+      debug(a,"   ",b)
+    end
+    
+    if received.fingerprint == expectedFingerprint --Normal, expected message
+      received.distance = received.distance + dist --Add on to repeater how far message had to go
+      debug("Sending Return Message")
+      modem.transmit(receivedFreq, replyFreq, received) --Send back exactly what we got
+      addID(received.id)
+    end
+
+    if #idMap > arbitraryNumber then --Purge messages to save memory
+      for i=#idMap, 2, -1 do --Save one ID for most recent message
+        sentMessages[idMap[i]] = nil
+        idMap[i] = nil
+      end
+    end
     
   end
 end
