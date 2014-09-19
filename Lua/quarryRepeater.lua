67d23888927a099611669030cb095c29c3e4f034
Civilwargeeky <civilwargeeky@yahoo.com>
2014-09-18 19:40:09 -0500

    Changing systems, added ping in repeater

--- Lua/quarryRepeater.lua
+++ Lua/quarryRepeater.lua
@@ -1,18 +1,7 @@
 --Version 1.0.0
 --This program will act as a repeater between a turtle and a receiver computer
---[[ How it works
-  First, it will startup, taking a list of channels to open in the argument
-  Then it will begin waiting for messages.
-  When it receives a message, it will check if the message is a table and has an id field.
-  If does not have an id field it will create one, taking a random number from 1 to 2 billion.
-  It checks if it has already sent this number.
-  If it has not sent the number, it will add the id to two or one lists
-    We could just have a list that is increasingly numbered with the ids, and go through all elements to check if sent
-    Or we could have a list where [id] = true, and a numbered list. Lookups will be done on the first list, and the second will be used for periodic deletions.
-  Then it will send the message along on the requested send channel.
-  If it gets a char event instead of a message, it will prompt the user to add more channels to open.
-  ]]
---expected message format: {message, id, distance}
+--important options are doAcceptPing and arbitraryNumber
+--expected message format: {message, id, distance, fingerprint}
 
 --Config
 local doDebug = true --...
@@ -20,14 +9,19 @@ local arbitraryNumber = 100 --How many messages to keep between deletions
 local saveFile = "QuarryRepeaterSave"
 local expectedFingerprint = "quarry"
 local acceptLegacy = true --This will auto-format old messages that come along
+local doAcceptPing = true --Accept pings. Can be turned off for tight quarters
+local pingFingerprint = "ping"
+local pingSide = "top"
 --Init
 local sentMessages = {} --Table of received message ids
-local idMap = {} --List of random ids
 local counter = 0
+local tempCounter = 0 --How many messages since last delete
+local recentID = 1 --Most recent message ID received, used for restore in delete
 local channels = {} --List of channels to open listen on
 local modem --The wireless modem
 
 
+--Function Declarations--
 local function debug(...) if doDebug then return print(...) end end --Debug print
 
 local function newID()
@@ -35,8 +29,9 @@ local function newID()
 end
 local function addID(id)
     sentMessages[id] = true
-    idMap[#idMap+1] =  id
+    tempCounter = tempCounter + 1
     counter = counter + 1
+    recentID = id
 end
 local function save()
   debug("Saving File")
@@ -55,6 +50,7 @@ local function openChannels(modem)
   end
 end
 
+--Actual Program Part Starts Here--
 if fs.exists(saveFile) then
   local file = fs.open(saveFile,"r")
   channels = textutils.unserialize(file:read()) or print("Channels could not be read") or {}
@@ -71,7 +67,7 @@ while not modem do
 end
 
 while true do
-  print("\nHit 'q' to quit, 'r' to remove channels, or any other key to add channels")
+  print("\nHit 'q' to quit, 'r' to remove channels, 'p' to ping or any other key to add channels")
   local event, key, receivedFreq, replyFreq, received, dist = os.pullEvent()
   term.clear()
   term.setCursorPos(1,1)
@@ -92,14 +88,15 @@ while true do
       debug("Sending Return Message")
       modem.transmit(receivedFreq, replyFreq, received) --Send back exactly what we got
       addID(received.id)
+    elseif doAcceptPing and received.fingerprint == pingFingerprint then --We got a ping!
+      redstone.setOutput(pingSide, not redstone.getOutput(pingSide)) --Just a toggle should be fine
     end
 
-    if #idMap > arbitraryNumber then --Purge messages to save memory
+    if tempCounter > arbitraryNumber then --Purge messages to save memory
       debug("Purging messages")
-      for i=#idMap, 2, -1 do --Save one ID for most recent message
-        sentMessages[idMap[i]] = nil
-        idMap[i] = nil
-      end
+      sleep(0.05) --Wait a tick for no good reason
+      sentMessages = {} --Completely reset table
+      sentMessages[recentID] = true --Reset last message (not sure if really needed. Oh well.
     end
     
     print("Messages Received: "..counter)
@@ -107,6 +104,10 @@ while true do
   elseif event == "char" then
     if key == "q" then --Quitting
       error("Quitting",0)
+    elseif key == "p" then --Ping other channels
+      for a,b in pairs(channels) do --Ping all open channels
+        modem.transmit(b,b,{message = "I am ping! Wrar!", fingerprint = "ping"})
+      end
     elseif key == "c" then --Removing Channels
       print("Enter a comma seperated list of channels to remove")
       local str = "" --Concantenate all the channels into one, maybe restructure this for sorting?
@@ -115,7 +116,7 @@ while true do
       end
       print("Channels: ",str:sub(1,-2)) --Sub for removing comma and space
       local input = io.read()
-      local toRemove = {} --We have to use this table because user list will not be in reverse numerical order
+      local toRemove = {} --We have to use this table because user list will not be in reverse numerical order. Because modifying while iterating is bad...
       for num in input:gmatch("%d+") do
         for a, b in pairs(channels) do
           if b == tonumber(num) then
