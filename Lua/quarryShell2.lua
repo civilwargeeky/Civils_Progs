73cb04054f662166466d7570ce45914f63253b8a
civilwargeeky <civilwargeeky@yahoo.com>
2015-02-05 13:41:30 -0600

    Fixing quarryShell

--- Lua/quarryShell2.lua
+++ Lua/quarryShell2.lua
@@ -5,6 +5,7 @@
 local dataFolder = "quarryData"
 
 local fileLocations = {}
+local fc = fileLocations
 fileLocations.quarry = "quarry.lua"
 
 local extensions = {}
@@ -22,6 +23,7 @@ local quarryFunctions = {}
 quarryFunctions.parseConfig = function(name) --This parses configs for other configs and returns full config
   local originalName = name --For recursion checking
   name = addDir(dataFolder, name..extensions.quarryConfig) --Put this in the proper place
+  print(name)
   if not fs.exists(name) and not fs.isDir(name) then return false, "file not found" end --Can't read what isn't there
   
   local file = fs.open(name, "r")
@@ -31,16 +33,21 @@ quarryFunctions.parseConfig = function(name) --This parses configs for other con
   local i = 1
   while true do
     local first, last = text:find("##[^#\n]+", i) --Double '#' means load config
+    print("Here")
     if not first then break end --No more matches
+    print("Got here")
     local str = text:sub(first+2, last) --Returns only the config name
     
+    local value
     if str:lower() ~= originalName:lower() then --So we don't call self. Not going to worry about other infinite recursion though, since its hard.
-      local value = quarryFunctions.parseConfig(str)
+      value = quarryFunctions.parseConfig(str)
+      print("Val: ",value)
     end
     if not value then
       value = "#Failed to load: "..str
     end
-    text = text:sub(0, first) .. value .. text:sub(last) --Take first part, add in value, then next part
+    text = text:sub(0, first-1) .. value .. text:sub(last+1) --Take first part, add in value, then next part
+    print(text)
     i = first + #value + 1 --If this exists, go to the end of it to parse
   end
   
@@ -49,6 +56,7 @@ end
 
 quarryFunctions.processConfig = function(name)
   local toSave, message = quarryFunctions.parseConfig(name)
+  error("Stop")
   if not toSave then return false, message end
   
   local file = fs.open(addDir(dataFolder, name.. extensions.quarryConfigFull), "w")
@@ -58,9 +66,14 @@ quarryFunctions.processConfig = function(name)
 end
 
 quarryFunctions.runConfig = function(name)
-  if processConfig(name) then --Prepare config for running
-    os.run(addDir(dataFolder, name..extensions.quarryConfigFull), {}) --Maybe later we'll handle running programs before/after quarry
+  if quarryFunctions.processConfig(name) then --Prepare config for running
+    print(fc.quarry.." -file "..addDir(dataFolder, name..extensions.quarryConfigFull))
+    local spoof = {} --This is to trick quarry into saving the proper file
+    spoof.getRunningProgram = function() return fc.quarry end
+    os.run({shell = spoof}, fc.quarry,"-file",addDir(dataFolder, name..extensions.quarryConfigFull)) --Maybe later we'll handle running programs before/after quarry
     return true
   end
   return false
 end
+
+quarryFunctions.runConfig("lol")
