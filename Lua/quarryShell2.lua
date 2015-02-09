b50c798ac853bbd4483c71f9ee28dbe584decdbe
civilwargeeky <civilwargeeky@yahoo.com>
2015-02-09 07:53:18 -0600

    Updated file checking, removed debug

--- Lua/quarryShell2.lua
+++ Lua/quarryShell2.lua
@@ -15,48 +15,41 @@ extensions.quarryConfigFull = ".qcf"
 local addDir = fs.combine
 
 local function loadFile(name, ext) --This should return a file handle and fileName
-  name = addDir(dataFolder, name..extensions.quarryConfig) --Put this in the proper place
+  name = addDir(dataFolder, name..ext) --Put this in the proper place
   if not fs.exists(name) and not fs.isDir(name) then return false end --Can't read what isn't there
+  return fs.open(name, "r"), name
 end
 
+--====================QUARRY FUNCTIONS====================
 local quarryFunctions = {}
-quarryFunctions.parseConfig = function(name) --This parses configs for other configs and returns full config
-  local originalName = name --For recursion checking
-  name = addDir(dataFolder, name..extensions.quarryConfig) --Put this in the proper place
-  print(name)
-  if not fs.exists(name) and not fs.isDir(name) then return false, "file not found" end --Can't read what isn't there
-  
-  local file = fs.open(name, "r")
+quarryFunctions.parseConfig = function(name) --This parses configs for other configs and returns full config  
+  local file = loadFile(name, ext)
+  if not file then return false, "file not found" end
   local text = file.readAll()
   file.close()
   
   local i = 1
   while true do
     local first, last = text:find("##[^#\n]+", i) --Double '#' means load config
-    print("Here")
     if not first then break end --No more matches
-    print("Got here")
     local str = text:sub(first+2, last) --Returns only the config name
     
     local value
-    if str:lower() ~= originalName:lower() then --So we don't call self. Not going to worry about other infinite recursion though, since its hard.
+    if str:lower() ~= name:lower() then --So we don't call self. Not going to worry about other infinite recursion though, since its hard.
       value = quarryFunctions.parseConfig(str)
-      print("Val: ",value)
     end
     if not value then
       value = "#Failed to load: "..str
     end
     text = text:sub(0, first-1) .. value .. text:sub(last+1) --Take first part, add in value, then next part
-    print(text)
+    print("Text:\n",text,"\n----------")
     i = first + #value + 1 --If this exists, go to the end of it to parse
   end
-  
   return text
 end
 
 quarryFunctions.processConfig = function(name)
   local toSave, message = quarryFunctions.parseConfig(name)
-  error("Stop")
   if not toSave then return false, message end
   
   local file = fs.open(addDir(dataFolder, name.. extensions.quarryConfigFull), "w")
@@ -75,5 +68,7 @@ quarryFunctions.runConfig = function(name)
   end
   return false
 end
+
+--====================MAIN PROGRAM====================
 
 quarryFunctions.runConfig("lol")
