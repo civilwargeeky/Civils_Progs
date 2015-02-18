0fc46e1fbc93bc8bafcdcdc4c1ea57bf0aa0c138
unknown <civilwargeeky@yahoo.com>
2015-02-18 14:01:48 -0600

    Made menu easier to read. Moved align to outside

--- Lua/menuAPI.lua
+++ Lua/menuAPI.lua
@@ -1,6 +1,7 @@
 --Menu API made by Civilwargeeky
 --Version 1.0.2
 
+--Text Utilities
 function titleize(text)
   local x = term.getSize()
   if #text+2 > x then return text:upper() end
@@ -10,6 +11,15 @@ function titleize(text)
   end
   return text
 end
+local function align(text, alignment, x) --Used to align text to a certain direction
+  if alignment == "left" then return 1 end
+  if alignment == "center" then return (x/2)-(#text/2) end
+  if alignment == "right" then return x - #text+1 end
+  error("Invalid Alignment",3) --Three because is only called by output
+end
+
+
+--Table Preparation Utilities
 function sentenceCaseTable(tab) --Expects a prepared table or sequentially numbered
   local toRet = {} --So we don't go modifying people's tables
   for a, b in pairs(tab) do
@@ -32,7 +42,6 @@ function prepareTable(tab)
 end
 
 function separateLines(text, x)
-  print("Starting this")
   local toRet = {text}
   if text:match("\n") then --We can't handle newlines inside this, so we'll split it up and do recursion!
     local pos = text:find("\n") --Get where the newline is
@@ -54,8 +63,8 @@ function separateLines(text, x)
       local cutString = curr:sub(1,x)..""
       local cutStart, sneakyEnd = cutString:find("%s+%S-$") --Apparently captures don't work in string.find. I'll just find the length of a substring *sigh*
       local cutEnd
-      if cutStart then --I don't know if sneakyEnd is necessary, it should always be end of string
-        cutEnd = cutStart + #(cutString:sub(cutStart, sneakyEnd):match("%s+")) - 1 --This just gets the number of spaces at the end of the line. Could be multiple
+      if cutStart then  --This just gets the number of spaces at the end of the line. Could be multiple
+        cutEnd = cutStart + #(cutString:sub(cutStart, sneakyEnd):match("%s+")) - 1 --I don't know if sneakyEnd is necessary, it should always be end of string
       end
       
       if cutStart or math.huge < x then --If there is a whitespace we can split at
@@ -69,108 +78,111 @@ function separateLines(text, x)
     i = i+1
   end
   
-  return toRet
+  return toRet, #toRet
 end
 
 function menu(title, description, textTable, isNumbered, titleAlign, textAlign, prefixCharacter, suffixCharacter, spaceCharacter, incrementFunction)
-local x, y = term.getSize() --Screen size
-local currIndex, scroll = 1, 0 --currIndex is the item from the table it is on, scroll is how many down it should go.
-local titleLines, descriptionLines = 0,0 --How many lines the title and description take up
-local alignments = { left = "left", center = "center", right = "right" } --Used for checking if alignment is valid
-if not (title and textTable) then error("Requires title and menu list",2) end
-if not type(textTable) == "table" and #textTable >= 1 then error("Menu list must be a table with values",2) end
-if isNumbered == nil then isNumbered = true end --Setting isNumbered default
-titleAlign = alignments[titleAlign] or alignments.center --Default title alignment
-textAlign = alignments[textAlign] or alignments.left --Default options alignment
-prefixCharacter = prefixCharacter or "["
-suffixCharacter = suffixCharacter or "]"
-spaceCharacter = spaceCharacter or "."
-for i=1, #textTable do
-  if type(textTable[i]) ~= "table" then 
-    textTable[i] = {text = textTable[i]} --If it is given without key and value pairs
-  end
-  textTable[i].text = textTable[i].text or textTable[i].value --This allows you to have tables of text, function pairs. So my function returns 1, and you call input[1].func()
-  textTable[i].key = textTable[i].key or i
-end
-local function align(text, alignment) --Used to align text to a certain direction
-  if alignment == "left" then return 1 end
-  if alignment == "center" then return (x/2)-(#text/2) end
-  if alignment == "right" then return x - #text+1 end
-  error("Invalid Alignment",3) --Three because is only called by output
-end
-
-local function output(text,y, alignment, assumeSingle) --My own term.write with more control
-  local originalAlignment, printTab, lines = alignment --Setting locals
-  if type(text) == "table" then --Assuming this is from separateLines
-    printTab, lines = text, #text
-  elseif assumeSingle then --Saves from doing separateLines on all the menu options
-    printTab, lines = {text}, 1
-  else
-    printTab, lines = separateLines(text)
-  end
-  for i=1, lines do
-    local x = align(printTab[i], alignment)
-    term.setCursorPos(x,y+i-1) ---1 because it will always be at least +1
-    term.clearLine()
-    term.write(printTab[i])
-    --term.write(" Writing to "..tostring(x)..","..tostring(y+i-1).." lines "..tostring(lines)) --Debug
-    --os.pullEvent("char")
-  end
-end
-
-title, titleLines = separateLines(title)
-if description then  --descriptionLines is how many lines the description takes up
-  description, descriptionLines = separateLines(description)
-end
-local upperLines = descriptionLines + titleLines --The title line, descriptions, plus extra line
-if upperLines > y-3 then error("Top takes up too many lines",2) end --So at least two options are on screen
-local top, bottom = 1, (y-upperLines) --These two are used to determine what options are on the screen right now (through scroll)
-while true do
-  while currIndex <= top and top > 1 do --If index is at top, scroll up
-    scroll = scroll - 1
-    top, bottom = top - 1, bottom - 1
-  end
-  while currIndex >= bottom and bottom < #textTable do --If at bottom scroll down. Change to > instead of >= to only do on bottom line. Same for above
-    scroll = scroll + 1
-    top, bottom = top + 1, bottom + 1
-  end
-  term.clear()
-  output(title,1, titleAlign) --Print title
-  if descriptionLines >= 1 then --Not an else because we don't want to print nothing
-    output(description,titleLines+1, titleAlign)
-  end
-  for i = 1, math.min(y - upperLines,#textTable) do --The min because may be fewer table entries than the screen is big
-    local prefix, suffix = "", "" --Stuff like spaces and numbers
-    if isNumbered then prefix = tostring(textTable[i+scroll].key)..spaceCharacter.." " end --Attaches a number to the front
-    if i + scroll == currIndex then prefix = prefixCharacter.." "..prefix; suffix = suffix.." "..suffixCharacter  --Puts brackets on the one highlighted
-      elseif textAlign == "left" then for i=1, #prefixCharacter+1 do prefix = " "..prefix end --This helps alignment
-      elseif textAlign == "right" then for i=1, #suffixCharacter+1 do suffix  = suffix.." " end --Same as above
+  --Initialization
+  local x, y = term.getSize() --Screen size
+  local currIndex, scroll = 1, 0 --currIndex is the item from the table it is on, scroll is how many down it should go.
+  local titleLines, descriptionLines = 0,0 --How many lines the title and description take up
+  local alignments = { left = "left", center = "center", right = "right" } --Used for checking if alignment is valid
+  
+  --Checking for all required parts
+  if not (title and textTable) then error("Requires title and menu list",2) end
+  if not type(textTable) == "table" and #textTable >= 1 then error("Menu list must be a table with values",2) end
+  if isNumbered == nil then isNumbered = true end --Setting isNumbered default
+  titleAlign = alignments[titleAlign] or alignments.center --Default title alignment
+  textAlign = alignments[textAlign] or alignments.left --Default options alignment
+  prefixCharacter = prefixCharacter or "["
+  suffixCharacter = suffixCharacter or "]"
+  spaceCharacter = spaceCharacter or "."
+  for i=1, #textTable do
+    if type(textTable[i]) ~= "table" then 
+      textTable[i] = {text = textTable[i]} --If it is given without key and value pairs
     end
-    local toPrint = prefix..textTable[i+scroll].text..suffix
-    if #toPrint > x then term.clear(); term.setCursorPos(1,1); error("Menu item "..tostring(i+scroll).." is longer than one line. Cannot Print",2) end
-    output(toPrint, i + upperLines, textAlign, true)
+    textTable[i].text = textTable[i].text or textTable[i].value --This allows you to have tables of text, function pairs. So my function returns 1, and you call input[1].func()
+    textTable[i].key = textTable[i].key or i
   end
-  if type(incrementFunction) ~= "function" then --This allows you to have your own custom logic for how to shift up and down and press enter. 
-    incrementFunction = defaultMenuKeyHandler--e.g. You could use redstone on left to increment, right to decrement, front to press enter.
-  end
-  local event = { os.pullEvent() } --Gets a nice table to pass event with
-  local action = incrementFunction(currIndex, #textTable, unpack(event))
-  if tonumber(action) then
-    local num = tonumber(action)
-    if num <= #textTable and num > 0 then
-      currIndex = num
+  
+  --Defining local functions
+  local function output(text,y, alignment, assumeSingle) --My own term.write with more control
+    local originalAlignment, printTab, lines = alignment --Setting locals
+    if type(text) == "table" then --Assuming this is from separateLines
+      printTab, lines = text, #text
+    elseif assumeSingle then --Saves from doing separateLines on all the menu options
+      printTab, lines = {text}, 1
+    else
+      printTab, lines = separateLines(text)
     end
-  elseif action == "up" and currIndex > 1 then
-    currIndex = currIndex - 1
-  elseif action == "down" and currIndex < #textTable then
-    currIndex = currIndex + 1
-  elseif action == "enter" then
-    return textTable[currIndex].text, textTable[currIndex].key, currIndex, textTable[currIndex].value
+    for i=1, lines do
+      local x = align(printTab[i], alignment, x) --Note inside is non-local x
+      term.setCursorPos(x,y+i-1) ---1 because it will always be at least +1
+      term.clearLine()
+      term.write(printTab[i])
+      --term.write(" Writing to "..tostring(x)..","..tostring(y+i-1).." lines "..tostring(lines)) --Debug
+      --os.pullEvent("char")
+    end
   end
-end
+
+  --Getting all initializations done
+  title, titleLines = separateLines(title)
+  if description then  --descriptionLines is how many lines the description takes up
+    description, descriptionLines = separateLines(description)
+  end
+  local upperLines = descriptionLines + titleLines --The title line, descriptions, plus extra line
+  if upperLines > y-3 then error("Top takes up too many lines",2) end --So at least two options are on screen
+  local top, bottom = 1, (y-upperLines) --These two are used to determine what options are on the screen right now (through scroll)
+  
+  --Main Loop
+  while true do
+    while currIndex <= top and top > 1 do --If index is at top, scroll up
+      scroll = scroll - 1
+      top, bottom = top - 1, bottom - 1
+    end
+    while currIndex >= bottom and bottom < #textTable do --If at bottom scroll down. Change to > instead of >= to only do on bottom line. Same for above
+      scroll = scroll + 1
+      top, bottom = top + 1, bottom + 1
+    end
+    term.clear()
+    output(title,1, titleAlign) --Print title
+    if descriptionLines >= 1 then --Not an else because we don't want to print nothing
+      output(description,titleLines+1, titleAlign)
+    end
+    for i = 1, math.min(y - upperLines,#textTable) do --The min because may be fewer table entries than the screen is big
+      local prefix, suffix = "", "" --Stuff like spaces and numbers
+      if isNumbered then prefix = tostring(textTable[i+scroll].key)..spaceCharacter.." " end --Attaches a number to the front
+      if i + scroll == currIndex then prefix = prefixCharacter.." "..prefix; suffix = suffix.." "..suffixCharacter  --Puts brackets on the one highlighted
+        elseif textAlign == "left" then for i=1, #prefixCharacter+1 do prefix = " "..prefix end --This helps alignment
+        elseif textAlign == "right" then for i=1, #suffixCharacter+1 do suffix  = suffix.." " end --Same as above
+      end
+      local toPrint = prefix..textTable[i+scroll].text..suffix
+      if #toPrint > x then term.clear(); term.setCursorPos(1,1); error("Menu item "..tostring(i+scroll).." is longer than one line. Cannot Print",2) end
+      output(toPrint, i + upperLines, textAlign, true)
+    end
+    
+    --Handle events
+    if type(incrementFunction) ~= "function" then --This allows you to have your own custom logic for how to shift up and down and press enter. 
+      incrementFunction = defaultMenuKeyHandler--e.g. You could use redstone on left to increment, right to decrement, front to press enter.
+    end
+    local action = incrementFunction(currIndex, #textTable) --Function handles calling os.pullEvent()
+    if tonumber(action) then
+      local num = tonumber(action)
+      if num <= #textTable and num > 0 then
+        currIndex = num
+      end
+    elseif action == "up" and currIndex > 1 then
+      currIndex = currIndex - 1
+    elseif action == "down" and currIndex < #textTable then
+      currIndex = currIndex + 1
+    elseif action == "enter" then
+      return textTable[currIndex].text, textTable[currIndex].key, currIndex, textTable[currIndex].value
+    end
+  end
 end
 
-function defaultMenuKeyHandler(index, maxIndex, event, key) --This handles events
+function defaultMenuKeyHandler(index, maxIndex) --This handles events
+  local event, key = os.pullEvent("key")
   if event == "key" then
     if key == 200 then --Up arrow
       if index == 1 then return maxIndex end --Go to bottom if at top
